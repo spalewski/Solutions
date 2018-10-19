d9ce722702fd2a666bc112dbb717f9ebfc51ede9
SebastianPal <spalewski@gmail.com>
2018-10-19 19:00:51 +0200

    Lions - WordsWithoutList

--- src/com/company/Lions/WordsWithoutList.java
+++ src/com/company/Lions/WordsWithoutList.java
@@ -1,5 +1,17 @@
 package com.company.Lions;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class WordsWithoutList {
 
+  public List wordsWithoutList(String[] words, int len) {
+    List score = new ArrayList();
+    for (int i = 0; i < words.length; i++) {
+      if (words[i].length() != len) {
+        score.add(words[i]);
+      }
+    }
+    return score;
+  }
 }
